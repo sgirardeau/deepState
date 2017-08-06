ea983a24ee909447a1a91a96baf0d9d2c8bf22e7
Jwmarion <jwmarion@gmail.com>
2017-08-05 23:06:54 -0400

    fleshed out beginning portion of game.js, started text parser and game loop

--- js/event.js
+++ js/event.js
@@ -13,3 +13,5 @@ class event{
     this.costs = costs;
   }
 }
+//variables are placeholders
+var exampleEvent = new Event('Kill dude', 'Go to the east side and kill dude, he sucks.', 4, cash[100], cash[-100], 4, 300,null,kill,0);
--- js/game.js
+++ js/game.js
@@ -6,6 +6,7 @@ var player = {
   agents: [],
   enemies: [],
   places: [],
+  events: [] //added events variable so we dont need another object just for it
   assets: {
     money: [0,0],
     influence: {
@@ -22,4 +23,95 @@ var player = {
     }
   },
   score: 0
+}
+
+
+
+
+
+console.log('☠ Deep State ☠'); //lets use skulls as much as possible ☠☠☠☠☠☠☠
+
+function listDirections(){
+    console.log('What will you do?\n Actors: 1    Events: 2   Quit Forever: 3');
+}
+function listEvents(){
+   console.API.clear()
+  console.log('Current events: (0 to go back)');
+  player.events.forEach((e,i)=>{
+    console.log((i+1) + ": " + e.title);
+  });
+}
+
+function listActors(){
+ console.API.clear()
+  console.log('Available actors: (0 to go back)');
+  player.events.forEach((e,i)=>{
+    console.log((i+1) + ": " + e.name);
+
+  });
+}
+
+function selectEvent(){
+  var selection, input;
+  eInput = raw_input();
+
+  if (eInput.parseInt() === 0){
+    //go back
+  } else
+  if eInput.parseInt() > 1 && eInput.parseInt() <= player.events - 1){
+    //run selection
+  } else
+  if eInput.parseInt() === NaN) {
+    //error, select valid number
+  }
+
+}
+
+function selectActor(){
+  var selection, input;
+  aInput = raw_input();
+
+  if (aInput.parseInt() === 0){
+    //go back
+  } else
+  if (aInput.parseInt() > 1 && aInput.parseInt() <= player.actors - 1){
+    //run selection
+  } else
+  if (aInput.parseInt() === NaN) {
+    //error, select valid number
+  }
+
+}
+
+
+
+
+
+//main game loop alpha
+
+//create basic variables
+  //roll initial characters
+  //roll first mission
+var continue = true;
+while(continue){
+  var input;
+
+  input = raw_input();
+
+  if (input.parseInt() === 1){
+    listActors();
+    selectActor();
+  } else
+  if (input.parseInt() === 2){
+    listEvents();
+    selectEvent();
+  } else
+  if (input.parseInt() === 3){
+    continue = false;
+  }
+  else{
+   console.API.clear()
+    console.log('Enter a valid command');
+    listDirections();
+  }
 }
